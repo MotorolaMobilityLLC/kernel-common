ec6cc470d5a33059d5e44f83b3ae25ef030154c9
Qiuliang Cheng <chengql2@motorola.com>
2022-11-14 12:45:07 +0800

    SM8550/Kernel: Add BUG_ON(1) in __alloc_pages_slowpath()
    kernel_platform/common/mm/page_alloc.c: add BUG_ON(1) in
    __alloc_pages_slowpath() failure route
    
    To assist the analysis of IPA setup failure because of Dma alloc
    failure, add debug code on the route of page alloc failure.
    
    To avoid fault triggers of BUG_ON(1), adjust the debug code execution
    condition as:
    - userdebug variant
    - current process is "netmgrd"
    - allocate buddy order is 2 (16KB) or 3 (32KB)
    - GFP flags with GFP_ATOMIC
    
    Change-Id: I415c106a9639508b82282d6cd485c558866d2e51
    Signed-off-by: Qiuliang Cheng <chengql2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2428620
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Zhenxin Xi <xizx@motorola.com>
    Submit-Approved: Jira Key

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5495,6 +5495,13 @@ fail:
 
 	warn_alloc(gfp_mask, ac->nodemask,
 			"page allocation failure: order:%u", order);
+#ifdef CONFIG_MMI_NON_GKI_DEBUG
+	// MMI_STOPSHIP Memory: Temporarily add debug info to check IPA issue about memory alloc failure
+	if(!strcmp("netmgrd", current->comm) && ((order == 2) || (order == 3)) && (gfp_mask & GFP_ATOMIC)) {
+		printk("mmi: trigger panic for netmgrd debug\n");
+		BUG_ON(1);
+	}
+#endif
 got_pg:
 	trace_android_vh_alloc_pages_slowpath(gfp_mask, order, alloc_start);
 	return page;
