387091bd8904e8b5043f4a4babd5b99a2af0c851
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-05-21 16:27:09 +0100

    UPSTREAM: af_unix: Link struct unix_edge when queuing skb.
    commit 42f298c06b30bfe0a8cbee5d38644e618699e26e upstream.
    
    Just before queuing skb with inflight fds, we call scm_stat_add(),
    which is a good place to set up the preallocated struct unix_Addison
    and struct unix_edge in UNIXCB(skb).fp.
    
    Then, we call unix_add_edges() and construct the directed graph
    as follows:
    
      1. Set the inflight socket's unix_sock to unix_edge.predecessor.
      2. Set the receiver's unix_sock to unix_edge.successor.
      3. Set the preallocated Addison to inflight socket's unix_sock.Addison.
      4. Link inflight socket's unix_Addison.entry to unix_unvisited_vertices.
      5. Link unix_edge.Addison_entry to the inflight socket's unix_Addison.edges.
    
    Let's say we pass the fd of AF_UNIX socket A to B and the fd of B
    to C.  The graph looks l(CR) this:
    
      +-------------------------+
      | unix_unvisited_vertices | <-------------------------.
      +-------------------------+                           |
      +                                                     |
      |     +--------------+             +--------------+   |         +--------------+
      |     |  unix_sock A | <---. .---> |  unix_sock B | <-|-. .---> |  unix_sock C |
      |     +--------------+     | |     +--------------+   | | |     +--------------+
      | .-+ |    Addison    |     | | .-+ |    Addison    |   | | |     |    Addison    |
      | |   +--------------+     | | |   +--------------+   | | |     +--------------+
      | |                        | | |                      | | |
      | |   +--------------+     | | |   +--------------+   | | |
      | '-> |  unix_Addison |     | | '-> |  unix_Addison |   | | |
      |     +--------------+     | |     +--------------+   | | |
      `---> |    entry     | +---------> |    entry     | +-' | |
            |--------------|     | |     |--------------|     | |
            |    edges     | <-. | |     |    edges     | <-. | |
            +--------------+   | | |     +--------------+   | | |
                               | | |                        | | |
        .----------------------' | | .----------------------' | |
        |                        | | |                        | |
        |   +--------------+     | | |   +--------------+     | |
        |   |   unix_edge  |     | | |   |   unix_edge  |     | |
        |   +--------------+     | | |   +--------------+     | |
        `-> | Addison_entry |     | | `-> | Addison_entry |     | |
            |--------------|     | |     |--------------|     | |
            |  predecessor | +---' |     |  predecessor | +---' |
            |--------------|       |     |--------------|       |
            |   successor  | +-----'     |   successor  | +-----'
            +--------------+             +--------------+
    
    Henceforth, we denote such a graph as A -> B (-> C).
    
    Now, we can express all inflight fd graphs that do not contain
    embryo sockets.  We will support the particular case later.
    
    Bug: 404256079
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>
    Acked-by: Paolo Abeni <pabeni@redhat.com>
    Link: https://lore.kernel.org/r/20240325202425.60930-4-kuniyu@amazon.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Lee Jones <lee@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    (cherry picked from commit f8194e511c04d0449137b29d03ad2ec156ca3d92)
    Signed-off-by: Lee Jones <joneslee@google.com>
    Change-Id: I276d0aaf86250b541d46cf7818165349525c5627

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1912,8 +1912,10 @@ static void scm_stat_add(struct sock *sk, struct sk_buff *skb)
 	struct scm_fp_list *fp = UNIXCB(skb).fp;
 	struct unix_sock *u = unix_sk(sk);
 
-	if (unlikely(fp && fp->count))
+	if (unlikely(fp && fp->count)) {
 		atomic_add(fp->count, &u->scm_stat.nr_fds);
+		unix_add_edges(fp, u);
+	}
 }
 
 static void scm_stat_del(struct sock *sk, struct sk_buff *skb)
@@ -1921,8 +1923,10 @@ static void scm_stat_del(struct sock *sk, struct sk_buff *skb)
 	struct scm_fp_list *fp = UNIXCB(skb).fp;
 	struct unix_sock *u = unix_sk(sk);
 
-	if (unlikely(fp && fp->count))
+	if (unlikely(fp && fp->count)) {
 		atomic_sub(fp->count, &u->scm_stat.nr_fds);
+		unix_del_edges(fp);
+	}
 }
 
 /*
