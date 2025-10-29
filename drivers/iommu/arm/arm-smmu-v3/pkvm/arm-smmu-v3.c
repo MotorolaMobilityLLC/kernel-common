1d9f6d1e54f47f796c0dee19725829c329a8d301
Mostafa Saleh <smostafa@google.com>
2025-10-29 14:12:34 +0000

    ANDROID: driver/arm-smmu-v3-kvm: Check domain attrs on detach
    Check that the detached STE matches the domain.
    
    Bug: 457592956
    Change-Id: I5b8c56984c0a13a8404746129c9d339525e7467b
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    (cherry picked from commit a47e0e78ad5b4e153b40fc1c9def11991aa6ca0c)
    Signed-off-by: Lee Jones <joneslee@google.com>
    (cherry picked from commit d0d9f251048481a85ac886c0a9101b170f37d078)

--- drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/pkvm/arm-smmu-v3.c
@@ -1365,6 +1365,7 @@ static int smmu_detach_dev(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_dom
 	struct hyp_arm_smmu_v3_domain *smmu_domain = domain->priv;
 	u32 pasid_bits = 0;
 	u64 *cd_table, *cd;
+	u32 domain_id, ste_cfg;
 
 	hyp_write_lock(&smmu_domain->list_lock);
 	kvm_iommu_lock(iommu);
@@ -1374,12 +1375,17 @@ static int smmu_detach_dev(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_dom
 		goto out_unlock;
 	}
 
+	ste_cfg = FIELD_GET(STRTAB_STE_0_CFG, dst->data[0]);
 	/*
 	 * For stage-1:
 	 * - The kernel has to detach pasid = 0 the last.
 	 * - This will free the CD.
 	 */
 	if (smmu_domain->type == KVM_ARM_SMMU_DOMAIN_S1) {
+		if (ste_cfg != STRTAB_STE_0_CFG_S1_TRANS) {
+			ret = -EACCES;
+			goto out_unlock;
+		}
 		pasid_bits = FIELD_GET(STRTAB_STE_0_S1CDMAX, dst->data[0]);
 		if (pasid >= (1 << pasid_bits)) {
 			ret = -E2BIG;
@@ -1403,7 +1409,12 @@ static int smmu_detach_dev(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_dom
 					goto out_unlock;
 				}
 			}
-
+			cd = smmu_get_cd_ptr(cd_table, 0);
+			domain_id = FIELD_GET(CTXDESC_CD_0_ASID, cd[0]);
+			if (domain->domain_id != domain_id) {
+				ret = -EACCES;
+				goto out_unlock;
+			}
 			smmu_free_cd(cd_table, pasid_bits);
 		} else {
 			cd = smmu_get_cd_ptr(cd_table, pasid);
@@ -1412,12 +1423,24 @@ static int smmu_detach_dev(struct kvm_hyp_iommu *iommu, struct kvm_hyp_iommu_dom
 				ret = -ENOENT;
 				goto out_unlock;
 			}
+			domain_id = FIELD_GET(CTXDESC_CD_0_ASID, cd[0]);
+			if (domain->domain_id != domain_id) {
+				ret = -EACCES;
+				goto out_unlock;
+			}
 			cd[0] = 0;
 			smmu_sync_cd(smmu, sid, pasid);
 			cd[1] = 0;
 			cd[2] = 0;
 			cd[3] = 0;
 			ret = smmu_sync_cd(smmu, sid, pasid);
+			goto out_unlock;
+		}
+	} else {
+		domain_id = FIELD_GET(STRTAB_STE_2_S2VMID, dst->data[2]);
+		if ((ste_cfg != STRTAB_STE_0_CFG_S2_TRANS) ||
+		    (domain->domain_id != domain_id)) {
+			ret = -EACCES;
 			goto out_unlock;
 		}
 	}
